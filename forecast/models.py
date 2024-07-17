880a2882359711b51c458a8515d5ea058c32d915
speedfreak <gdramze@gmail.com>
2024-07-17 14:12:17 +0300

    add frequent cities user cities

--- forecast/models.py
+++ forecast/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class FrequentRequest(models.Model):
+    city = models.CharField(max_length=50, verbose_name='Город')
+    count = models.PositiveIntegerField(default=1, verbose_name='Количество запросов')
+
+    class Meta:
+        ordering = ['count']
+
+
+class UserRequest(models.Model):
+    city = models.CharField(max_length=50, verbose_name='Город')
+    request_date = models.DateTimeField(auto_now_add=True, verbose_name='Время запроса')
+    user = models.ForeignKey('users.User', related_name='requests', on_delete=models.CASCADE, verbose_name='Пользователь')
+
+    class Meta:
+        ordering = ['-request_date']
--- forecast/views.py
+++ forecast/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from forecast.models import FrequentRequest, UserRequest
 from forecast.utils.get_json import DataGetter
 
 
@@ -9,8 +10,12 @@ def index(request):
 
     context = {
         'title': 'Главная',
+        'freq_requests': FrequentRequest.objects.all().order_by('-count')[:5],
         'has_info': False
     }
+
+    if request.user:
+        context['user_requests'] = UserRequest.objects.filter(user=request.user).order_by('-request_date')[:3]
 
     if city:
         data, city_name = DataGetter.get_forecast(city, days)
@@ -18,5 +23,17 @@ def index(request):
         context['data'] = data
         context['city'] = city_name
         context['has_info'] = True
+
+        # увеличение счетчика запросов
+        if FrequentRequest.objects.filter(city=city_name).exists():
+            freq_req = FrequentRequest.objects.get(city=city_name)
+            freq_req.count += 1
+            freq_req.save()
+        else:
+            FrequentRequest.objects.create(city=city_name)
+
+        # трекер пользовательских запросов
+        if request.user:
+            UserRequest.objects.create(city=city_name, user=request.user)
 
     return render(request, 'forecast/index.html', context)
